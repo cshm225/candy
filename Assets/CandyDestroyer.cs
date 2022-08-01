1c4df210f951ce91c96ca8223c408b033ba1a29d
cshm <cshm225@gmail.com>
2022-08-01 18:25:09 +0900

    4-6

--- Assets/CandyDestroyer.cs
+++ Assets/CandyDestroyer.cs
@@ -6,13 +6,21 @@ public class CandyDestroyer : MonoBehaviour
 {
     // Start is called before the first frame update
     public CandyManager candyManager;
+    public GameObject effectPrefab;
+    public Vector3 effectRotation;
+
     public int reward;
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag=="Candy")
+        if (other.gameObject.tag == "Candy")
         {
             Destroy(other.gameObject);
             candyManager.AddCandy(reward);
+            if (effectPrefab != null)
+            {
+                Instantiate(effectPrefab, other.transform.position, Quaternion.Euler(effectRotation));
+
+            }
         }
     }
 }
